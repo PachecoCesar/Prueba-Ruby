9f077e4bb25f25d9e49b66ed93bdc58ecb173be7
PachecoCesar <c86pacheco@gmail.com>
2019-08-21 14:26:43 -0400

    adding approval in menu

--- prueba-ruby.rb
+++ prueba-ruby.rb
@@ -42,8 +42,10 @@ while choice != 4
       unattended.each_with_index do|grades, index|
         count += 1 if grades == 'A' && index != 0
       end
-      if unattended[0] == 'Macarena'
-        puts "La alumna #{unattended [0]} tuvo #{count} inasistencias"
+      if unattended[0] == 'Macarena' && count == 1
+      puts "La alumna #{unattended [0]} tuvo #{count} inasistencia"
+      elsif count == 1
+        puts "El alumno #{unattended [0]} tuvo #{count} inasistencia"
       elsif unattended[0] == 'Rocio'
         puts "La alumna #{unattended [0]} tuvo #{count} inasistencias"
       else
@@ -52,27 +54,42 @@ while choice != 4
     end
   end
 
-  def approval()
+  def approval
     grades = read_alum('alumnos.csv')
-    grades.each do |sum_grades|
+    puts 'Ingresa una nota entre 0 y 10, al presionar enter la nota sera 5.0'
+    num = gets.chomp.to_f
+    while 0..10
+    sume.each do |sume_grades|
       sum = 0
+      total_grades = sum_grades.size -1
+      sum_grades.each_with_index do |grades, index|
+        sum += grades.to_i if index !=0
+      end
+      average = sum / total_grades.to_f
+      puts "El alumno #{sume_grades [0]}aprovo con nota #{average}" if num !=0 && average >= num
+      puts "El alumno #{sume_grades [0]}aprovo con notan#{average}" if num = 0 && average >= 5 
     end
+    end
+
+
+
+
   end
 
   case choice
-    when 1
-      average
-      puts '-----------------------------------------------------'
-    when 2
-      absence
-      puts '-----------------------------------------------------'
-    when 3
-      average
-      puts '-----------------------------------------------------'
-    when 4
-      puts '¡Hasta Pronto!'
-      else
-      puts ' Selecciona un numeros entre 1 y 4'
-      puts '-----------------------------------------------------'
+  when 1
+    average
+    puts '-----------------------------------------------------'
+  when 2
+    absence
+    puts '-----------------------------------------------------'
+  when 3
+    approval
+    puts '-----------------------------------------------------'
+  when 4
+    puts '¡Hasta Pronto!'
+  else
+    puts ' Selecciona un numeros entre 1 y 4'
+    puts '-----------------------------------------------------'
   end
 end
